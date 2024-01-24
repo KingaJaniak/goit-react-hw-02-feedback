214d6da22905fb9702f5751333c95bb1dd8c9d41
Kinga Janiak <janiak.kinga96@gmail.com>
2024-01-24 21:00:05 +0100

    prop

--- src/components/feedbackoption.js
+++ src/components/feedbackoption.js
@@ -1,4 +1,5 @@
 import { Btns, BtnStats } from './FeedbackApp.styled.js';
+import PropTypes from "prop-types";
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
@@ -10,4 +11,9 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
       ))}
     </Btns>
   );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array,
+  onLeaveFeedback: PropTypes.func,
 };
